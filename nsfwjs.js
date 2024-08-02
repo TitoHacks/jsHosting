4bfef046a2e078ded86ef58e30460fee6a34e394
TitoHacks <35603092+TitoHacks@users.noreply.github.com>
2024-08-02 18:33:34 +0200

    Update nsfwjs.js

--- nsfwjs.js
+++ nsfwjs.js
@@ -1,4 +1,11 @@
-import { NSFW_CLASSES } from "./nsfw_classes";
+
+export const NSFW_CLASSES: {[classId: number]: 'Drawing' | 'Hentai' | 'Neutral' | 'Porn' | 'Sexy'} = {
+  0: 'Drawing',
+  1: 'Hentai',
+  2: 'Neutral',
+  3: 'Porn',
+  4: 'Sexy'
+}
 
 declare global {
   namespace NodeJS {
